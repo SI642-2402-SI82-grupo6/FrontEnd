d5e4c593b55a99f1947aca94d31b1113e83898d7
Sergio Pecan <u20201a938@upc.edu.pe>
2024-11-03 16:16:21 -0500

    update 3/11/2024

--- src/components/invoice/InvoiceInformation.vue
+++ src/components/invoice/InvoiceInformation.vue
@@ -1,6 +1,67 @@
-<script >
+<template>
+  <Card>
+    <template #content>
+      <h3>Invoice Information</h3>
+      <div class="card flex justify-center">
+        <Toast />
+        <Form @submit.prevent="submitInvoice">
+          <div class="p-grid p-fluid">
+            <!-- Fecha Emisión -->
+            <div class="p-field p-col-12 p-md-6">
+              <div class="p-d-flex p-ai-center">
+                <label for="fechaEmision" class="p-mr-2" style="width: 120px;">Fecha Emisión:</label>
+                <Calendar id="fechaEmision" v-model="invoice.fechaEmision" required></Calendar>
+              </div>
+            </div>
+
+            <!-- Fecha Pago -->
+            <div class="p-field p-col-12 p-md-6">
+              <div class="p-d-flex p-ai-center">
+                <label for="fechaPago" class="p-mr-2" style="width: 120px;">Fecha Pago:</label>
+                <Calendar id="fechaPago" v-model="invoice.fechaPago" required></Calendar>
+              </div>
+            </div>
+
+            <!-- Total Facturado -->
+            <div class="p-field p-col-12 p-md-6">
+              <div class="p-d-flex p-ai-center">
+                <label for="totalFacturado" class="p-mr-2" style="width: 120px;">Total Facturado:</label>
+                <InputNumber
+                    id="totalFacturado"
+                    v-model="invoice.totalFacturado"
+                    mode="decimal"
+                    :minFractionDigits="0"
+                    :maxFractionDigits="4"
+                    required
+                ></InputNumber>
+              </div>
+            </div>
+
+            <!-- Retención -->
+            <div class="p-field p-col-12 p-md-6">
+              <div class="p-d-flex p-ai-center">
+                <label for="retencion" class="p-mr-2" style="width: 120px;">Retención:</label>
+                <InputNumber
+                    id="retencion"
+                    v-model="invoice.retencion"
+                    mode="decimal"
+                    :minFractionDigits="0"
+                    :maxFractionDigits="4"
+                    required
+                ></InputNumber>
+              </div>
+            </div>
+          </div>
+        </Form>
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script>
 import FinanceDataService from '../../services/FinanceDataService.js';
 import { reactive } from 'vue';
+
 export default {
   name: 'InvoiceInformation',
   data() {
@@ -26,71 +87,6 @@ export default {
 }
 </script>
 
-<template>
-  <Card>
-
-    <template #content>
-      <h3>Invoice Information</h3>
-      <div class="card flex justify-center">
-        <Toast />
-
-        <Form @submit.prevent="submitInvoice">
-          <div class="p-grid p-fluid">
-            <!-- Fecha Emisión -->
-            <div class="p-field p-col-12 p-md-6 p-formgrid p-fluid">
-              <label for="fechaEmision" class="p-col-fixed" style="width: 120px;">Fecha Emisión:</label>
-              <div class="p-col">
-                <Calendar id="fechaEmision" v-model="invoice.fechaEmision" required></Calendar>
-              </div>
-            </div>
-
-            <!-- Fecha Pago -->
-            <div class="p-field p-col-12 p-md-6 p-formgrid p-fluid">
-              <label for="fechaPago" class="p-col-fixed" style="width: 120px;">Fecha Pago:</label>
-              <div class="p-col">
-                <Calendar id="fechaPago" v-model="invoice.fechaPago" required></Calendar>
-              </div>
-            </div>
-
-            <!-- Total Facturado -->
-            <div class="p-field p-col-12 p-md-6 p-formgrid p-fluid">
-              <label for="totalFacturado" class="p-col-fixed" style="width: 120px;">Total Facturado:</label>
-              <div class="p-col">
-                <InputNumber
-                    id="totalFacturado"
-                    v-model="invoice.totalFacturado"
-                    mode="decimal"
-                    :minFractionDigits="0"
-                    :maxFractionDigits="4"
-                    required
-                ></InputNumber>
-              </div>
-            </div>
-
-            <!-- Retención -->
-            <div class="p-field p-col-12 p-md-6 p-formgrid p-fluid">
-              <label for="retencion" class="p-col-fixed" style="width: 120px;">Retención:</label>
-              <div class="p-col">
-                <InputNumber
-                    id="retencion"
-                    v-model="invoice.retencion"
-                    mode="decimal"
-                    :minFractionDigits="0"
-                    :maxFractionDigits="4"
-                    required
-                ></InputNumber>
-              </div>
-            </div>
-          </div>
-        </Form>
-
-
-      </div>
-    </template>
-  </Card>
-</template>
-
-
 <style scoped>
 form {
   display: flex;
@@ -103,17 +99,6 @@ form {
 
 label {
   font-weight: bold;
-}
-
-input {
-  padding: 0.5rem;
-  font-size: 1rem;
-}
-
-button {
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  cursor: pointer;
 }
 
 h3 {
