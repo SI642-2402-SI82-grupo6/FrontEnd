9f8e2e89e1f3f5c650a2f8f5b9099f268d92642f
Sergio Pecan <u20201a938@upc.edu.pe>
2024-11-17 19:43:45 -0500

    update 3/11/2024

--- src/components/wallet/WalletResult.vue
+++ src/components/wallet/WalletResult.vue
@@ -39,6 +39,11 @@ const fetchWalletResults = () => {
 const handleRowClick = (rowData) => {
   console.log('Datos de la fila:', rowData.data.documentoId);  // Verifica que rowData.data tiene los datos completos
   console.log('Tipo de documento:', rowData.data.type);  // Verifica que rowData.data tiene el tipo de documento
+  if (rowData.data.type === 'LETRA') {
+    router.push({ name: 'LetterDetail', params: { id: rowData.data.documentoId } });
+  } else if (rowData.data.type === 'FACTURA') {
+    router.push({ name: 'InvoiceDetail', params: { id: rowData.data.documentoId } });
+  }
 
 
 };
